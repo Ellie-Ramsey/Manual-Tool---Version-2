3a42f17e883ca07751850fc7df833f2614a70193
Ellie Jones <ellie@ramseysystems.co.uk>
2023-10-09 10:48:10 +0100

    Fixed Tab Code Removal - Test stuff woman

--- static/js/script.js
+++ static/js/script.js
@@ -11,22 +11,22 @@ let timelineNextId = 1;
 
 //////////////////////////NAVBAR//////////////////////////
 //MUltiple Tab Code
-function openPage(pageName,elmnt,color) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
+// function openPage(pageName,elmnt,color) {
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablink");
     
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-    }
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].style.backgroundColor = "";
+//     }
 
-    document.getElementById(pageName).style.display = "block";
-    elmnt.style.backgroundColor = color;
-  }
-  document.getElementById("defaultOpen").click();
+//     document.getElementById(pageName).style.display = "block";
+//     elmnt.style.backgroundColor = color;
+//   }
+//   document.getElementById("defaultOpen").click();
 
 
 ///////////////////////////STORY//////////////////////////

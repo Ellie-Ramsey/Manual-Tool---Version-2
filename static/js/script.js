ec83f3f529f4cdd0beb69ba3583f12fe09049471
Ellie Jones <ellie@ramseysystems.co.uk>
2023-10-11 12:51:35 +0100

    javascript tidy + readme update

--- static/js/script.js
+++ static/js/script.js
@@ -10,9 +10,7 @@ let standardDataPaths = {};
 let standardsData = {};
 
   
-
-
-//////////////////////////TextAreas Show/ Hide//////////////////////////
+//////////////////////////RELEVANT TO BOTH//////////////////////////
 document.getElementById('toggleStoryText').addEventListener('change', function() {
   var textArea = document.getElementById('storyTextArea');
   if(this.checked) {
@@ -32,16 +30,23 @@ document.getElementById('toggleTimelineText').addEventListener('change', functio
 });
 
 
+// New function to export both JSON files
+function exportAllJSON() {
+  exportStoryJSON();
+  exportTimelineJSON();
+}
+
+// Attach event listener to the "Save All JSON" button
+document.getElementById('saveAllJSONButton').addEventListener('click', exportAllJSON);
+
+
+
+
 ///////////////////////////STORY//////////////////////////
 //story text area update
 function updateStoryDisplay() {
   document.getElementById('storyTextArea').textContent = JSON.stringify(story_data, null, 2);
 }
-
-function updateTimelineDisplay() {
-  document.getElementById('timelineTextArea').textContent = JSON.stringify(timeline_data, null, 2);
-}
-
 
 //story story_data variable update
 document.getElementById("storyTableBody").addEventListener('input', function (e) {
@@ -51,40 +56,6 @@ document.getElementById("storyTableBody").addEventListener('input', function (e)
       story_data[key] = e.target.textContent;
       updateStoryDisplay();
   }
-});
-
- 
-//story history save
-$('#story-history-button').click(function() {
-  let headingVar = "storyHeading" + storySave;
-  let collapseVar = "storyCollapse" + storySave;
-  
-  let formattedStoryData = '';
-  
-  // Assuming story_data is a single object
-  for (let key in story_data) {
-    if (story_data.hasOwnProperty(key)) { // Check if the key exists in the object
-      formattedStoryData += `<strong>${key}:</strong> ${story_data[key]}<br/>`;
-    }
-  }
-  
-  let html_data = `
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="${headingVar}">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${collapseVar}" aria-expanded="false" aria-controls="${collapseVar}">
-          Accordion Item #${storySave}
-        </button>
-      </h2>
-      <div id="${collapseVar}" class="accordion-collapse collapse" aria-labelledby="${headingVar}" data-bs-parent="#accordionStory">
-        <div class="accordion-body">
-          ${formattedStoryData}
-        </div>
-      </div>
-    </div>
-  `;
-  
-  document.getElementById("accordionStory").innerHTML += html_data;
-  storySave++;
 });
 
 //story import code
@@ -118,10 +89,176 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
+//export individual JSON data
+function exportStoryJSON() {
+  const storyJSON = JSON.stringify(story_data, null, 2);
+  const blob = new Blob([storyJSON], { type: 'application/json' });
+  const a = document.createElement('a');
+  a.href = URL.createObjectURL(blob);
+  a.download = 'story.json';
+  a.click();
+  URL.revokeObjectURL(a.href);
+}
+
+
 
 ///////////////////////////TIMELINE//////////////////////////
+// timeline textarea e update
+function updateTimelineDisplay() {
+  document.getElementById('timelineTextArea').textContent = JSON.stringify(timeline_data, null, 2);
+}
 
-//timeline standards populate
+
+//export timeline code
+function downloadJSON(data, filename) {
+  const jsonData = JSON.stringify(data, null, 2);
+  const blob = new Blob([jsonData], { type: 'application/json' });
+  const a = document.createElement('a');
+  a.href = URL.createObjectURL(blob);
+  a.download = filename;
+  a.click();
+  URL.revokeObjectURL(a.href);
+}
+
+function exportTimelineJSON() {
+  // Transform main timeline
+  const newTimeline = transformMainTimeline(timeline_data);
+
+  // Download main timeline
+  downloadJSON(newTimeline, 'timeline.json');
+
+  // Generate and download linkedData files
+  const linkedDataFiles = generateLinkedDataFiles(timeline_data);
+  for (let filename in linkedDataFiles) {
+    downloadJSON(linkedDataFiles[filename], filename);
+  }
+}
+
+
+
+//timeline import code
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('importFiles').addEventListener('change', handleFilesUpload);
+  document.getElementById('processFilesBtn').addEventListener('click', processUploadedFiles);
+
+  let uploadedFiles = [];
+
+  function handleFilesUpload(event) {
+    uploadedFiles = event.target.files;
+    console.log(`${uploadedFiles.length} files uploaded. Press 'Process Files' to process.`);
+  }
+
+  function processUploadedFiles() {
+    let linkedDataFiles = [];
+
+    for (let file of uploadedFiles) {
+      if (file.name === 'timeline.json') {
+        parseTimelineFile(file, data => timeline_data = data);
+      } else if (file.name.startsWith('linkedData_')) {
+        linkedDataFiles.push(file);
+      }
+    }
+
+    const timelineDataParsedCheck = setInterval(() => {
+      if (timeline_data) {
+        clearInterval(timelineDataParsedCheck);
+        processLinkedDataFiles(linkedDataFiles, timeline_data);
+      }
+    }, 100);
+  }
+
+  function parseTimelineFile(file, callback) {
+    const reader = new FileReader();
+    reader.onload = e => {
+      const rawData = JSON.parse(e.target.result);
+      let structuredData = structureTimelineData(rawData);
+      callback(structuredData);
+    };
+    reader.onerror = err => console.error("Error reading file:", err);
+    reader.readAsText(file);
+  }
+
+  function processLinkedDataFiles(files, timeline_data) {
+    const promises = files.map(file => new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = e => {
+        try {
+          const linkedData = JSON.parse(e.target.result);
+          integrateLinkedDataToTimeline(linkedData, timeline_data, file.name);
+          resolve();
+        } catch (err) {
+          reject(new Error(`Error in parsing/processing linkedData from file ${file.name}: ${err.message}`));
+        }
+      };
+      reader.onerror = err => reject(new Error(`Error reading file ${file.name}: ${err.message}`));
+      reader.readAsText(file);
+    }));
+
+    Promise.all(promises)
+      .then(() => {
+        console.log("All linked data files processed.");
+        console.log("Final timeline data:", timeline_data);
+        renderTimelineTable();
+        updateTimelineDisplay(); // Ensure this function is defined in your code
+      })
+      .catch(err => console.error(err));
+  }
+  
+  function structureTimelineData(rawData) {
+    const structuredData = {};
+    rawData.forEach((item, index) => {
+        currentRowId++;  // Incrementing currentRowId for every item
+        timelineNextId = currentRowId + 1;  // Setting timelineNextId to be one more than currentRowId
+
+        structuredData[currentRowId] = {
+            id: currentRowId,
+            time: item.time,
+            event: item.event,
+            standard: null,
+            linkedData: []
+        };
+    });
+    return structuredData;
+}
+
+
+  function integrateLinkedDataToTimeline(linkedData, timeline_data, fileName) {
+    const linkedDataId = parseInt(fileName.split('_')[1], 10);
+
+    const standard = linkedData[0]?.standard || null;
+    if (timeline_data[linkedDataId]) {
+      timeline_data[linkedDataId].standard = standard;
+      timeline_data[linkedDataId].linkedData = linkedData.slice(1);
+    } else {
+      console.error(`Timeline data for linked data ID ${linkedDataId} not found.`);
+    }
+  }
+});
+
+//timeline variable to table render code (used for import)
+function renderTimelineTable() {
+  let tableContent = "";
+  for (let id in timeline_data) {
+    const rowData = timeline_data[id];
+    tableContent += `
+      <tr id="row-${id}">
+        <td data-key="time" contenteditable="true">${rowData.time}</td>
+        <td data-key="event" contenteditable="true">${rowData.event}</td>
+        <td>
+          <button id="dataItemBtn-${id}" onclick="toggleDataItem(${id})">${rowData.linkedData.length > 0 ? 'Edit' : 'Add'}</button>
+        </td>
+        <td>
+          <button class="btn btn-danger delete-row">Delete</button>
+        </td>
+      </tr>
+    `;
+  }
+  document.getElementById("timelineTableBody").innerHTML = tableContent;
+}
+
+
+
+
 function populateStandardsDropdown(standards) {
   console.log("Populating Standards: ", standards);
   const dropdown = document.getElementById('standardsDropdown');
@@ -166,95 +303,8 @@ function generateLinkedDataFiles(original) {
   return files;
 }
 
-function downloadJSON(data, filename) {
-  const jsonData = JSON.stringify(data, null, 2);
-  const blob = new Blob([jsonData], { type: 'application/json' });
-  const a = document.createElement('a');
-  a.href = URL.createObjectURL(blob);
-  a.download = filename;
-  a.click();
-  URL.revokeObjectURL(a.href);
-}
 
-function exportTimelineJSON() {
-  // Transform main timeline
-  const newTimeline = transformMainTimeline(timeline_data);
-
-  // Download main timeline
-  downloadJSON(newTimeline, 'timeline.json');
-
-  // Generate and download linkedData files
-  const linkedDataFiles = generateLinkedDataFiles(timeline_data);
-  for (let filename in linkedDataFiles) {
-    downloadJSON(linkedDataFiles[filename], filename);
-  }
-}
-
-// Existing functions to export individual JSON data
-function exportStoryJSON() {
-  const storyJSON = JSON.stringify(story_data, null, 2);
-  const blob = new Blob([storyJSON], { type: 'application/json' });
-  const a = document.createElement('a');
-  a.href = URL.createObjectURL(blob);
-  a.download = 'story.json';
-  a.click();
-  URL.revokeObjectURL(a.href);
-}
-
-
-// New function to export both JSON files
-function exportAllJSON() {
-  exportStoryJSON();
-  exportTimelineJSON();
-}
-
-// Attach event listener to the "Save All JSON" button
-document.getElementById('saveAllJSONButton').addEventListener('click', exportAllJSON);
-
-
-//timeline history save
-$('#timeline-history-button').click(function() {
-  let headingVar = "timelineHeading" + timelineSave;
-  let collapseVar = "timelineCollapse" + timelineSave;
-
-  let formattedTimelineData = formatTimelineDataForDisplay(timeline_data);
-
-  let html_data = `
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="${headingVar}">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${collapseVar}" aria-expanded="false" aria-controls="${collapseVar}">
-          Accordion Item #${timelineSave}
-        </button>
-      </h2>
-      <div id="${collapseVar}" class="accordion-collapse collapse" aria-labelledby="${headingVar}" data-bs-parent="#accordionTimeline">
-        <div class="accordion-body">
-          ${formattedTimelineData}
-        </div>
-      </div>
-    </div>
-  `;
-
-  document.getElementById("accordionTimeline").innerHTML += html_data;
-  timelineSave++;
-});
-
-function formatTimelineDataForDisplay(data, indent = 0) {
-  let formattedData = '';
-  for (let key in data) {
-    if (data.hasOwnProperty(key)) {
-      if (typeof data[key] === 'object' && data[key] !== null) {
-        formattedData += `${" ".repeat(indent)}<strong>${key}:</strong><br/>`;
-        formattedData += formatTimelineDataForDisplay(data[key], indent + 2);
-      } else {
-        formattedData += `${" ".repeat(indent)}<strong>${key}:</strong> ${data[key]}<br/>`;
-      }
-    }
-  }
-  return formattedData;
-}
-
-
-//timeline other
+//timeline main DOM
 document.addEventListener("DOMContentLoaded", function() {
   const linkedDataModal = document.getElementById("editModal");
   const closeLinkedData = document.getElementById("closeLinkedData");
@@ -479,13 +529,11 @@ function saveLinkedDataToJSON() {
   obj.standard = selectedStandard;  // Setting the standard
 }
 
-// IDK TBH
+// when linkeddatatable is no longer a fous, update JSON and timeline display
 document.getElementById("linkedDataTableBody").addEventListener("blur", function(event) {
   saveLinkedDataToJSON();
   updateTimelineDisplay();
 }, true);
-
-
 
 // close pop up table when clicked outside
 linkedDataModal.addEventListener("click", function(event) {
@@ -510,8 +558,6 @@ document.getElementById("linkedDataTableBody").addEventListener("change", functi
   }
 }, true);
 
-
-// BUG - THIS BOTTOM CODE IS BROKEN
 // update pop up table when standard changes
 document.getElementById('standardsDropdown').addEventListener('change', function(event) {
 
@@ -527,129 +573,4 @@ document.getElementById('standardsDropdown').addEventListener('change', function
   saveLinkedDataToJSON();
   updateTimelineDisplay();
 });
-
-
 });
-
-
-
-//timeline import code
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById('importFiles').addEventListener('change', handleFilesUpload);
-  document.getElementById('processFilesBtn').addEventListener('click', processUploadedFiles);
-
-  let uploadedFiles = [];
-
-  function handleFilesUpload(event) {
-    uploadedFiles = event.target.files;
-    console.log(`${uploadedFiles.length} files uploaded. Press 'Process Files' to process.`);
-  }
-
-  function processUploadedFiles() {
-    let linkedDataFiles = [];
-
-    for (let file of uploadedFiles) {
-      if (file.name === 'timeline.json') {
-        parseTimelineFile(file, data => timeline_data = data);
-      } else if (file.name.startsWith('linkedData_')) {
-        linkedDataFiles.push(file);
-      }
-    }
-
-    const timelineDataParsedCheck = setInterval(() => {
-      if (timeline_data) {
-        clearInterval(timelineDataParsedCheck);
-        processLinkedDataFiles(linkedDataFiles, timeline_data);
-      }
-    }, 100);
-  }
-
-  function parseTimelineFile(file, callback) {
-    const reader = new FileReader();
-    reader.onload = e => {
-      const rawData = JSON.parse(e.target.result);
-      let structuredData = structureTimelineData(rawData);
-      callback(structuredData);
-    };
-    reader.onerror = err => console.error("Error reading file:", err);
-    reader.readAsText(file);
-  }
-
-  function processLinkedDataFiles(files, timeline_data) {
-    const promises = files.map(file => new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = e => {
-        try {
-          const linkedData = JSON.parse(e.target.result);
-          integrateLinkedDataToTimeline(linkedData, timeline_data, file.name);
-          resolve();
-        } catch (err) {
-          reject(new Error(`Error in parsing/processing linkedData from file ${file.name}: ${err.message}`));
-        }
-      };
-      reader.onerror = err => reject(new Error(`Error reading file ${file.name}: ${err.message}`));
-      reader.readAsText(file);
-    }));
-
-    Promise.all(promises)
-      .then(() => {
-        console.log("All linked data files processed.");
-        console.log("Final timeline data:", timeline_data);
-        renderTimelineTable();
-        updateTimelineDisplay(); // Ensure this function is defined in your code
-      })
-      .catch(err => console.error(err));
-  }
-  
-  function structureTimelineData(rawData) {
-    const structuredData = {};
-    rawData.forEach((item, index) => {
-        currentRowId++;  // Incrementing currentRowId for every item
-        timelineNextId = currentRowId + 1;  // Setting timelineNextId to be one more than currentRowId
-
-        structuredData[currentRowId] = {
-            id: currentRowId,
-            time: item.time,
-            event: item.event,
-            standard: null,
-            linkedData: []
-        };
-    });
-    return structuredData;
-}
-
-
-  function integrateLinkedDataToTimeline(linkedData, timeline_data, fileName) {
-    const linkedDataId = parseInt(fileName.split('_')[1], 10);
-
-    const standard = linkedData[0]?.standard || null;
-    if (timeline_data[linkedDataId]) {
-      timeline_data[linkedDataId].standard = standard;
-      timeline_data[linkedDataId].linkedData = linkedData.slice(1);
-    } else {
-      console.error(`Timeline data for linked data ID ${linkedDataId} not found.`);
-    }
-  }
-});
-
-
-//timeline variable to table render code (used for import)
-function renderTimelineTable() {
-  let tableContent = "";
-  for (let id in timeline_data) {
-    const rowData = timeline_data[id];
-    tableContent += `
-      <tr id="row-${id}">
-        <td data-key="time" contenteditable="true">${rowData.time}</td>
-        <td data-key="event" contenteditable="true">${rowData.event}</td>
-        <td>
-          <button id="dataItemBtn-${id}" onclick="toggleDataItem(${id})">${rowData.linkedData.length > 0 ? 'Edit' : 'Add'}</button>
-        </td>
-        <td>
-          <button class="btn btn-danger delete-row">Delete</button>
-        </td>
-      </tr>
-    `;
-  }
-  document.getElementById("timelineTableBody").innerHTML = tableContent;
-}
